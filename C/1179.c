2afcddd2920d3fb0b7feb45fed33647cffd5306b
VINICIUS CALADO <vinicius.cl.dev@gmail.com>
2024-09-05 21:31:08 -0300

    Ignorando arquivos .exe

--- C/1179.c
+++ C/1179.c
@@ -8,32 +8,73 @@ int main(){
 		scanf("%d", &X);
 
 		if( X % 2 == 0){
-			for(int i = 0; i < 5; i++){
-				if(par[i] == -1){
-					if(i==5){
+			for(int i = 0; i < 5; i++)
+			{
+				if(par[i] == -1)
+				{
+					if(i == 4)
+					{
 						par[i] = X;
-						for(int i=0; i<5; i++)
-							printf("par[%d] = %d\n", i,par[i]);
-					}else{
+						for(int i = 0; i < 5; i++)
+						{
+							printf("par[%d] = %d\n", i, par[i]);
+							par[i] = -1;
+						}
+					}
+					else
+					{
+						printf("debug: par add\n");
 						par[i] = X;
 						break;
 					}				
 				}
 			}
-
 		}
 		else
 		{
-			for(int i = 0; i < 5; i++){
-				if(impar[i]== -1){
-					if(i==5){
+			for(int i = 0; i < 5; i++)
+			{
+				if(impar[i]== -1)
+				{
+					if(i==4)
+					{
 						impar[i] = X;
-						for(int i=0; i<5; i++){
-							printf("impar[%d] = %d", i, impar[i]);
+						for(int i=0; i<5; i++)
+						{
+							printf("impar[%d] = %d\n", i, impar[i]);
+							impar[i] = -1;
 						}
+					}
+					else
+					{
+						impar[i] = X;
+						break;
 					}
 				}
 			}
+		}
+	}
+	printf("DEBUG: RESTANTES DOS VETORES\n");
+	for(int i = 0; i < 5; i++)
+	{
+		if(impar[i]!= -1)
+		{
+			printf("impar[%d] = %d\n", i, impar[i]);
+		}
+		else
+		{
+			break;
+		}
+	}
+	for(int i=0; i<5; i++)
+	{
+		if(par[i] != -1)
+		{
+			printf("par[%d] = %d\n", i, par[i]);
+		}
+		else
+		{
+			break;
 		}
 	}
 }
---
+++ C/resumo.c
@@ -0,0 +1,42 @@
+//RESUMO
+
+//RECEBER UM CHAR, COR
+
+//FAÇA UM PROGRAMA QUE DESEJA TRÊS CORES E EXIBA O NOME
+
+/* #include <stdio.h>
+int main(){
+	char cor[50];
+
+
+	printf("DIGITE O NOME DA COR\n");
+	scanf("%s", cor);
+
+	printf("COR DIGITADA: %s\n", cor);
+}
+
+*/
+
+//FAÇA UM MENU COM 5 OPCOES, SENDO A 5 SAI.
+
+#include <stdio.h>
+int main(){
+
+	int opc=0;
+
+	while(1){
+
+		if(opc == 1)
+		{
+			printf("====MENU====\n");
+			printf("OPCAO 1\n");
+			printf("OPCAO 2\n");
+			printf("OPCAO 5\n");
+			printf("OPCAO 4\n");
+			printf("5.SAIR \n");
+			scanf("%d", &opc)
+		}
+		
+	}
+
+}
